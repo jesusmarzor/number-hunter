3caa78fcb049215e9fde7ee3a111cf6b5414003d
jesusmarzor <jesusmarzor28@gmail.com>
2023-07-31 22:15:18 +0200

    Add: use twitch hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,9 @@
-import useLocalStorage from "@/hooks/useLocalStorage"
-import { usernameDefault } from "@/utils/constants"
-import { useEffect, useState } from "react"
-import { User } from "@/utils/interfaces"
-import { UserType } from "@/utils/enums"
-import tmi from "tmi.js"
+import useTwitch from "./hooks/useTwitch"
 
 const myChannel = "#jesusmarzor"
 
 const App = () => {
-  const [currentUser, setCurrentUser] = useState<User | null>(null)
-  const [winnerUser, setWinnerUser] = useState<User | null>(null)
-  const { getUser, setUser, removeUser } = useLocalStorage()
-
-  useEffect( () => {
-    removeUser(UserType.current)
-    let winnerUser = getUser(UserType.winner)
-    if (winnerUser && winnerUser.channel === myChannel) {
-      setWinnerUser(getUser(UserType.winner))
-    }
-    const client = tmi.client({
-      channels: [myChannel]
-    })
-    client.connect()
-    client.on('message', (channel, tags, message, self) => {
-      if(self || tags.username === getUser(UserType.current)?.username) return;
-      let newNumber = Number(message)
-      if (newNumber) {
-        if ((newNumber === (Number(getUser(UserType.current)?.number ?? 0) + 1))) {
-          newNumber > Number(getUser(UserType.winner)?.number) && setWinnerUser(null)
-          let currentUser = {
-            channel: channel,
-            username: tags.username ?? usernameDefault,
-            number: message
-          }
-          setCurrentUser(currentUser)
-          setUser(UserType.current, currentUser)
-        } else {
-          let winnerNumber = Number(getUser(UserType.winner)?.number)
-          if (!winnerNumber || Number(getUser(UserType.current)?.number) > winnerNumber) {
-            let winnerUser =  getUser(UserType.current)
-            if (winnerUser) {
-              setWinnerUser(winnerUser)
-              setUser(UserType.winner, winnerUser)
-            }
-          }
-          setCurrentUser(null)
-          removeUser(UserType.current)
-        }
-      }
-    });
-  }, [])
+  const { currentUser, winnerUser } = useTwitch({channel: myChannel})
 
   return (
     <div className="w-full min-h-screen bg-black flex justify-center items-center text-white">
---
+++ src/hooks/useTwitch.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react"
+import useLocalStorage from "@/hooks/useLocalStorage"
+import { UserType } from "@/utils/enums"
+import { User } from "@/utils/interfaces"
+import { usernameDefault } from "@/utils/constants"
+import tmi from "tmi.js"
+
+interface props {
+    channel: string
+}
+
+const useTwitch = ({channel}: props) => {
+    const [currentUser, setCurrentUser] = useState<User | null>(null)
+    const [winnerUser, setWinnerUser] = useState<User | null>(null)
+    const { getUser, setUser, removeUser } = useLocalStorage()
+
+    useEffect( () => {
+        removeUser(UserType.current)
+        let winnerUser = getUser(UserType.winner)
+        if (winnerUser && winnerUser.channel === channel) {
+          setWinnerUser(getUser(UserType.winner))
+        }
+        const client = tmi.client({
+          channels: [channel]
+        })
+        client.connect()
+        client.on('message', (channel, tags, message, self) => {
+          if(self || tags.username === getUser(UserType.current)?.username) return;
+          let newNumber = Number(message)
+          if (newNumber) {
+            if ((newNumber === (Number(getUser(UserType.current)?.number ?? 0) + 1))) {
+              newNumber > Number(getUser(UserType.winner)?.number) && setWinnerUser(null)
+              let currentUser = {
+                channel: channel,
+                username: tags.username ?? usernameDefault,
+                number: message
+              }
+              setCurrentUser(currentUser)
+              setUser(UserType.current, currentUser)
+            } else {
+              let winnerNumber = Number(getUser(UserType.winner)?.number)
+              if (!winnerNumber || Number(getUser(UserType.current)?.number) > winnerNumber) {
+                let winnerUser =  getUser(UserType.current)
+                if (winnerUser) {
+                  setWinnerUser(winnerUser)
+                  setUser(UserType.winner, winnerUser)
+                }
+              }
+              setCurrentUser(null)
+              removeUser(UserType.current)
+            }
+          }
+        });
+      }, [])
+
+    return {currentUser, winnerUser}
+}
+
+export default useTwitch
