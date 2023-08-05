d3bc76f8052005879dd41c66c08aec9f3fd12d69
jesusmarzor <jesusmarzor28@gmail.com>
2023-08-05 23:34:39 +0200

    Fix: empty spaces

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -3,9 +3,6 @@ import { Form } from "@/components/Login/Form";
 import { Footer } from "@/components/Login/Footer";
 
 export const Login = () => {
-
-    
-
     return (
         <section className="flex flex-col justify-between items-center w-full h-full">
             <Header/>
